8235f780fc7d9719243db717ba21b4793347b4e2
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-08 16:28:10 +0800

    added logic for changing square sizes in grid

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector("#canvas");
 
-const squares = [];
+let squares = [];
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
+    square.style.height = "62.5px";
+    square.style.width = "62.5px";
     square.setAttribute("class", "square");
     square.addEventListener("mouseover", (e) => {
         square.classList.add("filled")
@@ -11,6 +13,31 @@ for (let i = 0; i < 256; i++) {
     squares.push(square);
 }
 
-squares.forEach((square) => {
-    canvas.appendChild(square);
+canvas.replaceChildren(...squares);
+
+function rerenderGrid(squaresPerSide) {
+    const squaresAmt = squaresPerSide * squaresPerSide;
+    const sizePerSquare = Math.round((1000 / squaresPerSide) * 1000) / 1000;
+
+    squares = [];
+    for (let i = 0; i < squaresAmt; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        squares.forEach((square) => {
+            canvas.appendChild(square);
+        });
+        square.style.height = sizePerSquare + "px";
+        square.style.width = sizePerSquare + "px";
+        square.addEventListener("mouseover", (e) => {
+            square.classList.add("filled")
+        });
+        squares.push(square);
+    }
+    canvas.replaceChildren(...squares);
+}
+
+const editSquaresBtn = document.querySelector("#edit-squares-btn");
+editSquaresBtn.addEventListener("click", (e) => {
+    const squareAmt = prompt("Enter number of squares per side");
+    rerenderGrid(squareAmt);
 });

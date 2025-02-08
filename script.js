5616bd14929e29003fdceb0f63421887bf122714
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-08 13:26:03 +0800

    set squares to grid

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector("#canvas");
 
 const squares = [];
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
     squares.push(square);

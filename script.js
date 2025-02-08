088928d8cd5af99e9ed6c8b2bcc1c097e7158314
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-08 13:15:21 +0800

    generated initial squares

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+const canvas = document.querySelector("#canvas");
+
+const squares = [];
+
+for (let i = 0; i < 16; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    squares.push(square);
+}
+
+console.log(squares);
+
+squares.forEach((square) => {
+    canvas.appendChild(square);
+});

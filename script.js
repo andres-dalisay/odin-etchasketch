a33915bd0a1679d7e80d6c33405da2af097ec0f5
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-02-09 01:45:23 +0800

    added toggle for progressive darkening

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const DEFAULT_SQUARES = 16;
 const CANVAS_SIZE = 720;
 const canvas = document.querySelector("#canvas");
 const rgbToggle = document.querySelector("#rgb-toggle");
+const pdToggle = document.querySelector("#darkening-toggle");
 
 function randomRGBValue() {
     return Math.floor(Math.random() * 256);
@@ -19,9 +20,20 @@ function renderGrid(squaresPerSide) {
         square.style.width = sizePerSquare + "px";
         square.addEventListener("mouseover", (e) => {
             if (!square.classList.contains("filled")){
-                if (rgbToggle.checked)
-                    square.style.backgroundColor = "rgb(" + randomRGBValue() + "," + randomRGBValue() + "," + randomRGBValue() + ")"
-                    square.classList.add("filled");
+                if (rgbToggle.checked){
+                    square.style.backgroundColor = "rgb(" + randomRGBValue() + "," + randomRGBValue() + "," + randomRGBValue() + ")";
+                }
+                square.classList.add("filled");
+                if (pdToggle.checked) {
+                    square.style.opacity = 0.1;
+                }
+                
+            } else {
+                if (parseFloat(getComputedStyle(square).opacity) < 1 && pdToggle.checked) {
+                    square.style.opacity = parseFloat(getComputedStyle(square).opacity) + 0.1;
+                } else if (!pdToggle.checked) {
+                    square.style.opacity = 1;
+                }
             }
         });
         canvas.appendChild(square);
